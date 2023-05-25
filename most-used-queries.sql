a13a7baff084455f353455af6c86e8751e03d431
Kyungpyo Park <toheavener01@gmail.com>
2023-05-25 12:56:31 +0900

    Added a spilling metric in most-used-queries.

--- most-used-queries.sql
+++ most-used-queries.sql
@@ -24,3 +24,18 @@ order by 2 desc
 -- select * 
 --   from most_used_queries
 -- order by 2 desc
+
+-- Added. For spilling detection
+-- with spilled_queries as (
+-- select query_info.query_hashes.normalized_literals as hash_id, sum(js.shuffle_output_bytes_spilled) as sum_of_spilled
+--   from `region-asia-northeast3`.INFORMATION_SCHEMA.JOBS_BY_PROJECT, unnest(job_stages) js
+--  group by query_info.query_hashes.normalized_literals
+-- ),
+-- most_used_queries as (
+-- select query_info.query_hashes.normalized_literals as hash_id, count(1) as used_count, sum(total_slot_ms) as total_slot_sum, sum(total_bytes_processed) as total_bytes_sum, array_agg(query order by total_slot_ms desc limit 1)[safe_offset(0)] as query
+--   from `region-asia-northeast3`.INFORMATION_SCHEMA.JOBS_BY_PROJECT
+--  group by query_info.query_hashes.normalized_literals
+-- )
+-- select * 
+--   from most_used_queries join spilled_queries using(hash_id)
+-- order by 2 desc
